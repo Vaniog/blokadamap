56bd9c89e71a168a82521b7c41897c40186da7d4
SkreckOne <gorkov-00-00@mail.ru>
2023-12-27 08:51:13 +0300

    fixed bd

--- app/authors/models.py
+++ app/authors/models.py
@@ -7,78 +7,85 @@ from sqlalchemy import (
     Text,
     UniqueConstraint,
     PrimaryKeyConstraint,
+    Table,
+    Column
 )
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 from app.base.models import *
 
+# M2M-------------------------------------------------------------------------------------------
 
-class Many2ManyAuthorsBase(Base):
-    __abstract__ = True
-    author_id = mapped_column(
-        "author_id", SmallInteger, ForeignKey("author.author_id"), nullable=False
-    )
+Author2FamilyStatus = Table('author_to_family_status', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('family_status_id', SmallInteger, ForeignKey('family_status.family_status_id'), primary_key=True, nullable=False)
+)
+
+
+Author2Religion = Table('author_to_religion', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('religion_id', SmallInteger, ForeignKey('religion.religion_id'), primary_key=True, nullable=False)
+)
+
+Author2PoliticalParty = Table('author_to_political_party', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('political_party_id', SmallInteger, ForeignKey('political_party.political_party_id'), primary_key=True, nullable=False)
+)
+
+Author2SocialClass = Table('author_to_social_class', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('social_class_id', SmallInteger, ForeignKey('social_class.social_class_id'), primary_key=True, nullable=False)
+)
+
+Author2Nationality = Table('author_to_nationality', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('nationality_id', SmallInteger, ForeignKey('nationality.nationality_id'), primary_key=True, nullable=False)
+)
+
+Author2Education = Table('author_to_education', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('education_id', SmallInteger, ForeignKey('education.education_id'), primary_key=True, nullable=False)
+)
+
+Author2Occupation = Table('author_to_occupation', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('occupation_id', SmallInteger, ForeignKey('occupation.occupation_id'), primary_key=True, nullable=False)
+)
+
+Author2Card = Table('author_to_card', Base.metadata,
+    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+    Column('card_id', SmallInteger, ForeignKey('card.card_id'), primary_key=True, nullable=False)
+)
+
+Author2Point = Table('author_to_point', Base.metadata,
+                    Column('author_id', SmallInteger, ForeignKey('author.author_id'), primary_key=True, nullable=False),
+                    Column('point_id', SmallInteger, ForeignKey('point.point_id'), primary_key=True, nullable=False),
+                    Column('from', Date, nullable=False),
+                    Column('to', Date, nullable=False),
+                    Column('description', Text, nullable=False)
+)
 
 
 # Uniqe models--------------------------------------------------------------------------------------
 class Author(Base):
     __tablename__ = "author"
-    author_id = mapped_column(
-        "author_id", SmallInteger, primary_key=True, autoincrement=True, nullable=False
-    )
-    first_name = mapped_column("first_name", VARCHAR(63), nullable=False)
-    middle_name = mapped_column("middle_name", VARCHAR(63))
-    last_name = mapped_column("last_name", VARCHAR(63), nullable=False)
-    sex = mapped_column("sex", CHAR(1), nullable=False)
-    birth_date = mapped_column("birth_date", Date, nullable=False)
-    biography = mapped_column("biography", Text, nullable=False, unique=True)
+    author_id = mapped_column(SmallInteger, primary_key=True, nullable=False)
+    first_name = mapped_column(VARCHAR(63), nullable=False)
+    middle_name = mapped_column(VARCHAR(63))
+    last_name = mapped_column(VARCHAR(63), nullable=False)
+    sex = mapped_column(CHAR(1), nullable=False)
+    birth_date = mapped_column(Date, nullable=False)
+    biography = mapped_column(Text, nullable=False, unique=True)
 
-    def __init__(
-        self,
-        first_name: VARCHAR,
-        middle_name: VARCHAR,
-        last_name: VARCHAR,
-        sex: CHAR,
-        birth_date: Date,
-        biography: Text,
-    ):
-        self.first_name = first_name
-        self.middle_name = middle_name
-        self.last_name = last_name
-        self.sex = sex
-        self.birth_date = birth_date
-        self.biography = biography
-
-
-class AuthorToPoint(Base):
-    __tablename__ = "author_to_point"
-    author_id = mapped_column(
-        "author_id", SmallInteger, ForeignKey("author.author_id"), nullable=False
-    )
-    point_id = mapped_column(
-        "point_id", SmallInteger, ForeignKey("point.point_id"), nullable=False
-    )  # DONT FORGET TO EDIT
-    date_start = mapped_column("from", Date, nullable=False)
-    date_end = mapped_column("to", Date, nullable=False)
-    description = mapped_column("description", Text, nullable=False)
-    __table_args__ = (
-        UniqueConstraint("author_id", "point_id", name="_author_to_point_uc"),
-        PrimaryKeyConstraint('author_id', 'point_id'),
-    )
-
-    def __init__(
-        self,
-        author_id: SmallInteger,
-        point_id: SmallInteger,
-        date_start: Date,
-        date_end: Date,
-        description: Text,
-    ) -> None:
-        self.author_id = author_id
-        self.point_id = point_id
-        self.date_start = date_start
-        self.date_end = date_end
-        self.description = description
-
+    family_status = relationship('FamilyStatus', secondary=Author2FamilyStatus, back_populates='author')
+    religion = relationship('Religion', secondary=Author2Religion, back_populates='author')
+    political_party = relationship('PoliticalParty', secondary=Author2PoliticalParty, back_populates='author')
+    social_classes = relationship('SocialClass', secondary=Author2SocialClass, back_populates='author')
+    nationality = relationship('Nationality', secondary=Author2Nationality, back_populates='author')
+    education = relationship('Education', secondary=Author2Education, back_populates='author')
+    occupation = relationship('Occupation', secondary=Author2Occupation, back_populates='author')
+    card = relationship('Card', secondary=Author2Card, back_populates='author')
+    point = relationship('Point', secondary=Author2Point, back_populates='author')
 
 # ExtendedBaseClass-----------------------------------------------------------------------------------
 class FamilyStatus(ExtendedBaseClass):
@@ -91,6 +98,8 @@ class FamilyStatus(ExtendedBaseClass):
         nullable=False,
     )
 
+    authors = relationship('Author', secondary=Author2FamilyStatus, back_populates='family_status')
+
 
 class PoliticalParty(ExtendedBaseClass):
     __tablename__ = "political_party"
@@ -101,6 +110,7 @@ class PoliticalParty(ExtendedBaseClass):
         autoincrement=True,
         nullable=False,
     )
+    authors = relationship('Author', secondary=Author2PoliticalParty, back_populates='family_status')
 
 
 class Religion(ExtendedBaseClass):
@@ -112,6 +122,7 @@ class Religion(ExtendedBaseClass):
         autoincrement=True,
         nullable=False,
     )
+    authors = relationship('Author', secondary=Author2Religion, back_populates='religion')
 
 
 class SocialClass(ExtendedBaseClass):
@@ -124,6 +135,8 @@ class SocialClass(ExtendedBaseClass):
         nullable=False,
     )
 
+    authors = relationship('Author', secondary=Author2SocialClass, back_populates='social_class')
+
 
 class Nationality(ExtendedBaseClass):
     __tablename__ = "nationality"
@@ -134,6 +147,7 @@ class Nationality(ExtendedBaseClass):
         autoincrement=True,
         nullable=False,
     )
+    authors = relationship('Author', secondary=Author2Nationality, back_populates='nationality')
 
 
 class Education(ExtendedBaseClass):
@@ -145,6 +159,7 @@ class Education(ExtendedBaseClass):
         autoincrement=True,
         nullable=False,
     )
+    authors = relationship('Author', secondary=Author2Education, back_populates='education')
 
 
 class Occupation(ExtendedBaseClass):
@@ -156,6 +171,7 @@ class Occupation(ExtendedBaseClass):
         autoincrement=True,
         nullable=False,
     )
+    authors = relationship('Author', secondary=Author2Occupation, back_populates='occupation')
 
 
 class Card(ExtendedBaseClass):
@@ -163,152 +179,4 @@ class Card(ExtendedBaseClass):
     card_id = mapped_column(
         "card_id", SmallInteger, primary_key=True, autoincrement=True, nullable=False
     )
-
-
-# M2M--------------------------------------------------------------------------------------------------------------------
-class Author2FamilyStatus(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_family_status"
-    family_status_id = mapped_column(
-        "family_status_id",
-        SmallInteger,
-        ForeignKey("family_status.family_status_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "author_id", "family_status_id", name="_author_to_family_status_id_uc"
-        ),
-        PrimaryKeyConstraint('author_id', 'family_status_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, family_status_id: SmallInteger):
-        self.author_id = author_id
-        self.family_status_id = family_status_id
-
-
-class Author2PoliticalParty(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_political_party"
-    political_party_id = mapped_column(
-        "political_party_id",
-        SmallInteger,
-        ForeignKey("political_party.political_party_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "author_id", "political_party_id", name="_author_to_political_party_uc"
-        ),
-        PrimaryKeyConstraint('author_id', 'political_party_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, political_party_id: SmallInteger):
-        self.author_id = author_id
-        self.political_party_id = political_party_id
-
-
-class Author2Religion(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_religion_id"
-    religion_id = mapped_column(
-        "religion_id", SmallInteger, ForeignKey("religion.religion_id"), nullable=False
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "author_id", "religion_id", name="_author_to_religion_uc"
-        ),
-        PrimaryKeyConstraint('author_id', 'religion_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, religion_id: SmallInteger):
-        self.author_id = author_id
-        self.religion_id = religion_id
-
-
-class Author2SocialClass(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_social_class"
-    social_class_id = mapped_column(
-        "social_class_id",
-        SmallInteger,
-        ForeignKey("social_class.social_class_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "author_id", "social_class_id", name="_author_to_social_class_uc"
-        ),
-        PrimaryKeyConstraint('author_id', 'social_class_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, social_class_id: SmallInteger):
-        self.author_id = author_id
-        self.social_class_id = social_class_id
-
-
-class Author2Nationality(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_nationality"
-    nationality_id = mapped_column(
-        "nationality_id",
-        SmallInteger,
-        ForeignKey("nationality.nationality_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "author_id", "nationality_id", name="_author_to_nationality_uc"
-        ),
-        PrimaryKeyConstraint('author_id', 'nationality_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, nationality_id: SmallInteger):
-        self.author_id = author_id
-        self.nationality_id = nationality_id
-
-
-class Author2Education(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_education"
-    education_id = mapped_column(
-        "education_id",
-        SmallInteger,
-        ForeignKey("education.education_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint("author_id", "education_id", name="_author_to_education_uc"),
-        PrimaryKeyConstraint('author_id', 'education_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, education_id: SmallInteger):
-        self.author_id = author_id
-        self.education_id = education_id
-
-
-class Author2Occupation(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_occupation_id"
-    occupation_id = mapped_column(
-        "occupation_id",
-        SmallInteger,
-        ForeignKey("occupation.occupation_id"),
-        nullable=False,
-    )
-    __table_args__ = (
-        UniqueConstraint("author_id", "occupation_id", name="_author_to_occupation_uc"),
-        PrimaryKeyConstraint('author_id', 'occupation_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, occupation_id: SmallInteger):
-        self.author_id = author_id
-        self.occupation_id = occupation_id
-
-
-class Author2Card(Many2ManyAuthorsBase):
-    __tablename__ = "author_to_card_id"
-    card_id = mapped_column(
-        "card_id", SmallInteger, ForeignKey("card.card_id"), nullable=False
-    )
-    __table_args__ = (
-        UniqueConstraint("author_id", "card_id", name="_author_to_card_uc"),
-        PrimaryKeyConstraint('author_id', 'card_id'),
-    )
-
-    def __init__(self, author_id: SmallInteger, card_id: SmallInteger):
-        self.author_id = author_id
-        self.card_id = card_id
+    authors = relationship('Author', secondary=Author2Card, back_populates='card')
