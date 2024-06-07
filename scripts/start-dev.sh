f18c04535095130da3956c880899df6ead38fd95
Vaniog <forvantar@gmail.com>
2024-06-07 16:43:46 +0300

    remove exec from start-dev.sh

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,4 +3,4 @@
 APP_HOST=${HOST:-0.0.0.0}
 APP_PORT=${APP_PORT:-8080}
 
-exec uvicorn app.main:app --reload --proxy-headers --host "$APP_HOST" --port "$APP_PORT"
+uvicorn app.main:app --reload --proxy-headers --host "$APP_HOST" --port "$APP_PORT"
